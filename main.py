5c4ff7d816a7a0f926c2f0e700e1beb48439cd36
SANGON BRANDON <89807019+Chevalier-du-web@users.noreply.github.com>
2023-07-13 18:22:06 +0200

    structure du projet....
    structure du projet....

---
+++ main.py
@@ -0,0 +1,26 @@
+# Class principale de lancement du projet ....
+
+# importations des dependances ...
+from tkinter import *
+from pages.authentication.login import LoginPage
+
+class MainApp:
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry("1250x670+50+17")
+        self.root.title("UACLIB")
+        self.root.resizable(height=False, width=False)
+        # self.root.iconbitmap("")
+
+        # Appel de la page login ...
+        LoginPage(self.root,1250,670)
+
+        # affichage de la fenetre ...
+        self.root.mainloop()
+
+
+# instanciation de la classe principale ....
+
+windows = MainApp()
+
+print(windows)
---
+++ pages/authentication/login.py
@@ -0,0 +1,21 @@
+#importation des dependences ...
+from tkinter import *
+
+class LoginPage:
+    def __init__(self,root,width,height):
+        self.frame = Canvas(root,width=width, height=height)
+
+        # frame 1
+        frame1 = Canvas(self.frame,width=width//2, height=height,bg='purple')
+
+
+        frame1.place(x=0,y=0)
+
+        # frame 2
+        frame2 = Canvas(self.frame, width=width // 2, height=height,bg='red')
+
+
+        frame2.place(x=width//2, y=0)
+
+        # affichage de de la page ...
+        self.frame.place(x=0, y=0)
