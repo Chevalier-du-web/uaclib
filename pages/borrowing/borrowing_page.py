269ec7b9edb6e53b40705a90aa618e676c52be00
SANGON BRANDON <89807019+Chevalier-du-web@users.noreply.github.com>
2023-07-14 23:57:00 +0200

    ..

--- pages/borrowing/borrowing_page.py
+++ pages/borrowing/borrowing_page.py
@@ -1,9 +1,30 @@
 #importation des dependences ...
 from tkinter import *
+from tkinter import ttk
+
+from components.style import Style
+
 
 class BorrowingPage:
     def __init__(self,root,width,height):
-        self.frame = Canvas(root,width=width, height=height)
+        self.frame = Canvas(root,width=width, height=height,bg='lightblue')
+        Label(self.frame,text="Borrowing - Page",font=Style.font4_i,bg='lightblue').place(x=50,y=20)
+        Button(self.frame,font=('Arial',12,'italic'),text='           Add new borrow           ',command=self.frame.destroy).place(x=678,y=120)
+
+        # define columns
+        columns = ('id', 'username', 'email', 'level')
+
+        table = ttk.Treeview(self.frame, columns=columns, show='headings')
+
+        # define headings
+        table.heading('id', text='Username')
+        table.heading('username', text='Book')
+        table.heading('email', text='Start date')
+        table.heading('level', text='End date')
+
+        table.place(x=90, y=170)
+
+        Button(self.frame,font=('Arial',12,'italic'),text='           back           ',command=self.frame.destroy).place(x=40,y=500)
 
         # affichage de de la page ...
         self.frame.place(x=0, y=0)
