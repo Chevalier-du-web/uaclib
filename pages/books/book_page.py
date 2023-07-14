389a63027d7774664c69db45e03b9384aa128c8c
SANGON BRANDON <89807019+Chevalier-du-web@users.noreply.github.com>
2023-07-14 13:45:22 +0200

    add books pages

--- pages/books/book_page.py
+++ pages/books/book_page.py
@@ -1,10 +1,26 @@
 from tkinter import *
+from tkinter import ttk
+
+from components.style import Style
+
 
 class BooksPage:
     def __init__(self,root,width,height):
         self.frame = Canvas(root,width=width, height=height,bg='lightblue')
+        Label(self.frame,text="Book -page",font=Style.font4_i,bg='lightblue').place(x=50,y=20)
 
+        # define columns
+        columns = ('title', 'author', 'subject', 'quantity')
 
+        table = ttk.Treeview(self.frame, columns=columns, show='headings')
+
+        # define headings
+        table.heading('title', text='Title')
+        table.heading('author', text='Author')
+        table.heading('subject', text='Subject')
+        table.heading('quantity', text='Quantity')
+
+        table.place(x=90, y=150)
 
         # affichage de de la page ...
         self.frame.place(x=0, y=0)
--- pages/users/users_page.py
+++ pages/users/users_page.py
@@ -1,9 +1,28 @@
 from tkinter import *
+from tkinter import ttk
+from components.style import Style
+
 
 class UsersPage:
     def __init__(self,root,width,height):
         self.frame = Canvas(root,width=width, height=height,bg='lightblue')
+        Label(self.frame,text="All users",font=Style.font4_i,bg='lightblue').place(x=50,y=20)
 
+        # define columns
+        columns = ('id','username', 'email', 'level')
+
+        table = ttk.Treeview(self.frame, columns=columns, show='headings')
+
+        # define headings
+        table.heading('id', text='ID')
+        table.heading('username', text='Username')
+        table.heading('email', text='Email')
+        table.heading('level', text='Level')
+
+
+
+
+        table.place(x=90,y=120)
 
 
         # affichage de de la page ...
