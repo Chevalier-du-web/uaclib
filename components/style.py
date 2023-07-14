6d92125e1c2219cbb488c702bb531e5073ca544a
SANGON BRANDON <89807019+Chevalier-du-web@users.noreply.github.com>
2023-07-14 12:06:29 +0200

    ..

---
+++ components/style.py
@@ -0,0 +1,13 @@
+
+class Style:
+    font1_i = ("Arial",15,'italic')
+    font2_i = ("Arial", 16, 'italic')
+    font3_i = ("Arial", 17, 'italic')
+    font4_i = ("Arial", 18, 'italic')
+
+    # bold style ...
+    font1_b = ("Arial", 15, 'bold')
+    font2_b = ("Arial", 16, 'bold')
+    font3_b = ("Arial", 17, 'bold')
+    font4_b = ("Arial", 18, 'bold')
+    font5_b = ("Arial", 25, 'bold')
