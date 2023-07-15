4d7dcf62775de1fce27f98efc6a76e3033d4b2a3
annick <annickdominiqueh@gmail.com>
2023-07-15 16:45:53 +0200

    modification dans register.py
    modif  (juste pour changer l'emplacement des labels)

--- pages/authentication/register.py
+++ pages/authentication/register.py
@@ -1,5 +1,7 @@
 #importation des dependences ...
 from tkinter import *
+from tkinter import ttk
+
 
 class RegisterPage:
     def __init__(self,root,width,height):
@@ -20,57 +22,69 @@ class RegisterPage:
         heading.place(x=200,y=50)
         
         #######username
-        username_lbl=Label(frame2,text="Your username",bg="white",font=("Microsoft Yahei UI Light",11))
-        username_lbl.place(x=190,y=120)
+        username_lbl=Label(frame2,text="Username",bg="white",font=("Microsoft Yahei UI Light",11))
+        username_lbl.place(x=100,y=120)
         
         username_entry=Entry(frame2,highlightthickness=0,relief=FLAT,bg="white",fg="black",font=("Microsoft Yahei UI Light",11))
-        username_entry.place(x=190,y=160,width=250)
+        username_entry.place(x=210,y=120,width=250)
         
         username_line= Canvas(frame2,width=250,height=2.0,bg="pink",highlightthickness=0)
-        username_line.place(x=190,y=185)
+        username_line.place(x=210,y=142)
         
-        #######password
-        password_lbl=Label(frame2,text="Your password",bg="white",font=("Microsoft Yahei UI Light",11))
-        password_lbl.place(x=190,y=210)
-        
-        password_entry=Entry(frame2,highlightthickness=0,relief=FLAT,bg="white",fg="black",font=("Microsoft Yahei UI Light",11))
-        password_entry.place(x=190,y=250,width=250)
-        
-        password_line= Canvas(frame2,width=250,height=2.0,bg="pink",highlightthickness=0)
-        password_line.place(x=190,y=273)
-        
-        ##########gender
-        gender_lbl=Label(frame2,text="Your gender",bg="white",font=("Microsoft Yahei UI Light",11))
-        gender_lbl.place(x=190,y=300)
-        
-        gender_entry=Entry(frame2,highlightthickness=0,relief=FLAT,bg="white",fg="black",font=("Microsoft Yahei UI Light",11))
-        gender_entry.place(x=190,y=340,width=250)
-        
-        gender_line= Canvas(frame2,width=250,height=2.0,bg="pink",highlightthickness=0)
-        gender_line.place(x=190,y=370)
-        
-        #####level
-        level_lbl=Label(frame2,text="Your level",bg="white",font=("Microsoft Yahei UI Light",11))
-        level_lbl.place(x=190,y=400)
-        
-        level_entry=Entry(frame2,highlightthickness=0,relief=FLAT,bg="white",fg="black",font=("Microsoft Yahei UI Light",11))
-        level_entry.place(x=190,y=440,width=250)
-        
-        level_line= Canvas(frame2,width=250,height=2.0,bg="pink",highlightthickness=0)
-        level_line.place(x=190,y=470)
-        
-        ######email
-        email_lbl=Label(frame2,text="Your email",bg="white",font=("Microsoft Yahei UI Light",11))
-        email_lbl.place(x=190,y=500)
+        #######Email
+        email_lbl=Label(frame2,text="Email",bg="white",font=("Microsoft Yahei UI Light",11))
+        email_lbl.place(x=100,y=180)
         
         email_entry=Entry(frame2,highlightthickness=0,relief=FLAT,bg="white",fg="black",font=("Microsoft Yahei UI Light",11))
-        email_entry.place(x=190,y=540,width=250)
+        email_entry.place(x=210,y=180,width=250)
         
         email_line= Canvas(frame2,width=250,height=2.0,bg="pink",highlightthickness=0)
-        email_line.place(x=190,y=570)
+        email_line.place(x=210,y=200)
+        
+        ######password
+        password_lbl=Label(frame2,text="Password",bg="white",font=("Microsoft Yahei UI Light",11))
+        password_lbl.place(x=100,y=240)
+        
+        password_entry=Entry(frame2,highlightthickness=0,relief=FLAT,bg="white",fg="black",font=("Microsoft Yahei UI Light",11))
+        password_entry.place(x=210,y=238,width=250)
+        
+        password_line= Canvas(frame2,width=250,height=2.0,bg="pink",highlightthickness=0)
+        password_line.place(x=210,y=261)
+        
+        ##########level
+        
+        levels = ['Level 1', 'Level 2', 'Level 3', 'Master 1', 'Master 2', 'Doctora', 'Others']
+        level_lbl=Label(frame2,text="Level",bg="white",font=("Microsoft Yahei UI Light",11))
+        level = ttk.Combobox(frame2, font=("Microsoft Yahei UI Light",11),values=levels,width=28)
+        level_lbl.place(x=100,y=295)
+        level.place(x=210,y=293)
+        
+        level_line= Canvas(frame2,width=250,height=2.0,bg="pink",highlightthickness=0)
+        level_line.place(x=210,y=318)
+        
+        #####gender
+        genders = ['Male', 'Female']
+        gender_lbl=Label(frame2,text="Gender",bg="white",font=("Microsoft Yahei UI Light",11))
+        gender = ttk.Combobox(frame2, font=("Microsoft Yahei UI Light",11),values=genders,width=28)
+        gender_lbl.place(x=100,y=350)
+        gender.place(x=210,y=350)
+        
+        level_line= Canvas(frame2,width=250,height=2.0,bg="pink",highlightthickness=0)
+        level_line.place(x=210,y=374)
+        
+        
+        ####phone
+        phone_lbl=Label(frame2,text="Phone",bg="white",font=("Microsoft Yahei UI Light",11))
+        phone_lbl.place(x=100,y=400)
+        
+        phone_entry=Entry(frame2,highlightthickness=0,relief=FLAT,bg="white",fg="black",font=("Microsoft Yahei UI Light",11))
+        phone_entry.place(x=210,y=400,width=250)
+        
+        phone_line= Canvas(frame2,width=250,height=2.0,bg="pink",highlightthickness=0)
+        phone_line.place(x=210,y=420)
         
         ####button
-        Button(frame2,width=30,pady=7,text='Register',bg='#57a1f8',fg='white',cursor='hand2',border=0).place(x=200,y=600)
+        Button(frame2,width=30,pady=7,text='Register',bg='#57a1f8',fg='white',cursor='hand2',border=0).place(x=200,y=500)
         
         
         
