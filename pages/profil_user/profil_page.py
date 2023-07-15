1e5a0c5bac97eea9a018530c9f12ba3b57a176c4
SANGON BRANDON <89807019+Chevalier-du-web@users.noreply.github.com>
2023-07-16 01:49:23 +0200

    update profil

--- pages/profil_user/profil_page.py
+++ pages/profil_user/profil_page.py
@@ -3,7 +3,7 @@ from PIL import ImageTk,Image
 
 from backend.request_file import Request
 from components.style import Style
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 
 
 class ProfilPage:
@@ -22,16 +22,16 @@ class ProfilPage:
         self.frame_info = Canvas(self.frame,width=550,height=450,bg='teal')
 
         # infos for current user...
-        Label(self.frame_info,text="Username :  Sangon Brandon",font=Style.font3_i,bg='teal',fg='white').place(x=30,y=50)
-        Label(self.frame_info,text="Email :  brandon@gmail.com",font=Style.font3_i,bg='teal',fg='white').place(x=30,y=100)
-        Label(self.frame_info,text="password :  Brandon123",font=Style.font3_i,bg='teal',fg='white').place(x=30,y=150)
-        Label(self.frame_info,text="Level :  2",font=Style.font3_i,bg='teal',fg='white').place(x=30,y=200)
-        Label(self.frame_info,text="Sex :  Male",font=Style.font3_i,bg='teal',fg='white').place(x=30,y=250)
-        Label(self.frame_info,text="Phone :  657591936",font=Style.font3_i,bg='teal',fg='white').place(x=30,y=300)
+        Label(self.frame_info,text=f"Username :  {data[0][1]}",font=Style.font3_i,bg='teal',fg='white').place(x=60,y=50)
+        Label(self.frame_info,text=f"Email :  {data[0][2]}",font=Style.font3_i,bg='teal',fg='white').place(x=60,y=100)
+        Label(self.frame_info,text=f"password :  {data[0][5]}",font=Style.font3_i,bg='teal',fg='white').place(x=60,y=150)
+        Label(self.frame_info,text=f"Level :  {data[0][3]}",font=Style.font3_i,bg='teal',fg='white').place(x=60,y=200)
+        Label(self.frame_info,text=f"Sex :  {data[0][-1]}",font=Style.font3_i,bg='teal',fg='white').place(x=60,y=250)
+        Label(self.frame_info,text=f"Phone :  {data[0][4]}",font=Style.font3_i,bg='teal',fg='white').place(x=60,y=300)
 
 
         Button(self.frame_info,text="       back       ",font=Style.font2_b,command=self.frame.destroy).place(x=60,y=350)
-        Button(self.frame_info,text="    edit profil    ",font=Style.font2_b).place(x=340,y=350)
+        Button(self.frame_info,text="    edit profil    ",font=Style.font2_b,command=self.edit_user).place(x=340,y=350)
 
 
 
@@ -50,4 +50,43 @@ class ProfilPage:
     def openfile(self):
         file = filedialog.askopenfilename()
         self.photo = str(file)
+
+    def edit_user(self):
+        self.frame_edit = Canvas(self.frame, width=550, height=450, bg='teal')
+
+        # infos for current user...
+        Label(self.frame_edit, text=f"Username :  ", font=Style.font3_i, bg='teal', fg='white').place(x=60,y=50)
+        self.username = Entry(self.frame_edit,font=Style.font2_i)
+        self.username.place(x=200,y=50)
+
+        Label(self.frame_edit, text=f"Email :  ", font=Style.font3_i, bg='teal', fg='white').place(x=60,y=100)
+        self.email = Entry(self.frame_edit, font=Style.font2_i)
+        self.email.place(x=200, y=100)
+
+        Label(self.frame_edit, text=f"password :  ", font=Style.font3_i, bg='teal', fg='white').place(x=60,y=150)
+        self.password = Entry(self.frame_edit, font=Style.font2_i)
+        self.password.place(x=200, y=150)
+
+        diplomes = ['Level 1', 'Level 2', 'Level 3', 'Master 1', 'Master 2', 'Doctora', 'Others']
+        Label(self.frame_edit, font=Style.font3_i, text='Level : ', bg='teal').place(x=60, y=200)
+        self.level = ttk.Combobox(self.frame_edit, font=Style.font1_i, values=diplomes, width=20)
+        self.level.current(0)
+        self.level.place(x=200, y=200)
+
+        Label(self.frame_edit, text=f"Sex :  ", font=Style.font3_i, bg='teal', fg='white').place(x=60,y=250)
+        self.sex = ttk.Combobox(self.frame_edit, font=Style.font1_i, values=['Male', 'Female'], width=20)
+        self.sex.current(0)
+        self.sex.place(x=200, y=250)
+
+        Label(self.frame_edit, text=f"Phone :  ", font=Style.font3_i, bg='teal', fg='white').place(x=60,y=300)
+        self.phone = Entry(self.frame_edit, font=Style.font2_i)
+        self.phone.place(x=200, y=300)
+
+        Button(self.frame_edit, text="       back       ", font=Style.font2_b, command=self.frame.destroy).place(x=60,y=350)
+        Button(self.frame_edit, text="    Save profil    ", font=Style.font2_b,command=self.save_profil).place(x=340, y=350)
+
+        self.frame_edit.place(x=40, y=80)
+
+    def save_profil(self):
+        request = ""
 
